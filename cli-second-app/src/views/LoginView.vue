ef63f052dca03fbbcf46675c3cc7ebf29bb4da2c
minzzz1st7 <minzzz1st7@gmail.com>
2022-05-14 03:24:11 +0900

    첫번째

---
+++ cli-second-app/src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>로그인 화면입니다.</h2>
+    <div class="tcolor">나중에 만들겠음</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style scoped>
+.tcolor {
+  color: salmon;
+}
+</style>
